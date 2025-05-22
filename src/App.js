614099686ef77d6ed0a8430287990f7743d82e5b
Krishnamraja <krishnamrajathirupathi@gmail.com>
2025-05-22 19:52:47 +0530

    Add Chat bot

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Layout from "./Pages/Layout";
 import AutoCarousel from "./Pages/Carousel";
 import Cards from "./Pages/cards";
 import MentorshipCard from "./Pages/MentorshipCard";
+import Chatbox from "./ChatBox";
 function App() {
 
 
   return (
     <Layout>
       <AutoCarousel />
+      <Chatbox/>
       <MentorshipCard />
       <Cards />
     </Layout>
